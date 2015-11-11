5b5b70aa7e22e29f60d20e6b6e4d2d4ee8666767
Michael Hazell <mike.hazell@holler.com.au>
2015-11-12 08:43:35 +1100

    Rewrite
    Untested, and no docs updates.
    - now works as a decorator function
    - onload event is not attached if a callback name is given

--- src/async-script-loader.js
+++ src/async-script-loader.js
@@ -1,169 +1,164 @@
 import React, { PropTypes } from "react";
 
-let SCRIPT_MAP = new Map();
+const SCRIPT_MAP = new Map();
 
 // A counter used to generate a unique id for each component that uses the function
 let idCount = 0;
 
-export default function makeAsyncScript(Component, scriptURL, options) {
-  options = options || {};
-  let funcs = {
-    displayName: "AsyncScriptLoader",
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
 
-    propTypes: {
-      asyncScriptOnLoad: PropTypes.func,
-    },
-
-    statics: {
-      asyncScriptLoaderTriggerOnScriptLoaded() {
-        let mapEntry = SCRIPT_MAP.get(scriptURL);
-        if (!mapEntry || !mapEntry.loaded) {
-          throw new Error("Script is not loaded.");
-        }
-        for (let observer of mapEntry.observers.values()) {
-          observer(mapEntry);
-        }
-        delete window[options.callbackName];
-      },
-    },
-
-    getInitialState() {
-      return {};
-    },
-
-    asyncScriptLoaderGetScriptLoaderID() {
-      if (!this.__scriptLoaderID) {
-        this.__scriptLoaderID = "async-script-loader-" + idCount++;
-      }
-      return this.__scriptLoaderID;
-    },
-
-    getComponent() {
-      return this.childComponent;
-    },
-
-    componentDidMount() {
-      const key = this.asyncScriptLoaderGetScriptLoaderID();
-      const { globalName, callbackName } = options;
-      if (globalName && typeof window[globalName] !== "undefined") {
-        SCRIPT_MAP.set(scriptURL, { loaded: true, observers: new Map() });
-      }
-
-      if (SCRIPT_MAP.has(scriptURL)) {
-        let entry = SCRIPT_MAP.get(scriptURL);
-        if (entry.loaded || entry.errored) {
-          this.asyncScriptLoaderHandleLoad(entry);
-          return;
-        }
-        entry.observers.set(key, this.asyncScriptLoaderHandleLoad);
-        return;
-      }
-
-      let observers = new Map();
-      observers.set(key, this.asyncScriptLoaderHandleLoad);
-      SCRIPT_MAP.set(scriptURL, {
-        loaded: false,
-        observers: observers,
-      });
-
-      let script = document.createElement("script");
-
-      script.src = scriptURL;
-      script.async = 1;
-
-      let callObserverFuncAndRemoveObserver = (func) => {
-        if (SCRIPT_MAP.has(scriptURL)) {
-          let mapEntry = SCRIPT_MAP.get(scriptURL);
-          let observersMap = mapEntry.observers;
-
-          for (let [obsKey, observer] of observersMap) {
-            if (func(observer)) {
-              observersMap.delete(obsKey);
-            }
-          }
-        }
-
-        // Remove the global callback if it exists.
-        if (callbackName && typeof window !== "undefined") {
-            delete window[callbackName];
-        }
-      };
-
-      let scriptOnLoad = function(){
-        let mapEntry = SCRIPT_MAP.get(scriptURL);
-        mapEntry.loaded = true;
-        callObserverFuncAndRemoveObserver( (observer) => {
-          observer(mapEntry);
-          return true;
-        });
-      }
-
-      if (callbackName && typeof window !== "undefined") {
-        // If user has provided a callbackName, allow the script being loaded
-        // to trigger the callback.
-        window[callbackName] = scriptOnLoad;
-      } else {
-        // Otherwise listen fot the scripts onLoad event.
-        script.onload = scriptOnLoad;
-      }
-
-      script.onerror = (event) => {
-        let mapEntry = SCRIPT_MAP.get(scriptURL);
-        mapEntry.errored = true;
-        callObserverFuncAndRemoveObserver( (observer) => {
-          observer(mapEntry);
-          return true;
-        });
-      };
-
-      // (old) MSIE browsers may call "onreadystatechange" instead of "onload"
-      script.onreadystatechange = () => {
-        if (this.readyState === "loaded") {
-          // wait for other events, then call onload if default onload hadn't been called
-          window.setTimeout(() => {
-            if (SCRIPT_MAP.get(scriptURL).loaded !== true) {
-              script.onload();
-            }
-          }, 0);
-        }
-      };
-
-      document.body.appendChild(script);
-    },
-
-    asyncScriptLoaderHandleLoad(state) {
-      this.setState(state, this.props.asyncScriptOnLoad);
-    },
-
-    componentWillUnmount() {
-      // Clean the observer entry
-      let mapEntry = SCRIPT_MAP.get(scriptURL);
-      if (mapEntry) {
-        mapEntry.observers.delete(this.asyncScriptLoaderGetScriptLoaderID());
-      }
-    },
-
-    render() {
-      const globalName = options.globalName;
-      let { asyncScriptOnLoad, ...childProps } = this.props;
-      if (globalName && typeof window !== "undefined") {
-        childProps[globalName] = typeof window[globalName] !== "undefined" ? window[globalName] : undefined;
-      }
-      return <Component ref={(comp) => {this.childComponent = comp; }} {...childProps} />;
-    },
-
-  };
-
-  if (options.exposeFuncs) {
-    for (let funcToExpose of options.exposeFuncs) {
-      /* eslint-disable no-loop-func */
-      funcs[funcToExpose] = function() {
-        return this.childComponent[funcToExpose](...arguments);
-      };
-      /* eslint-enable no-loop-func */
+export default function makeAsyncScript({scriptURL, globalName, callbackName, exposeFuncs}) {
+    if (!scriptURL) {
+        throw new Error("makeAsyncScript requries a scriptURL");
     }
-  }
-  const AsyncScriptLoader = React.createClass(funcs);
 
-  return AsyncScriptLoader;
+    return function wrapWithAsyncScript(WrappedComponent) {
+        class AsyncScriptLoader extends React.Component {
+            static displayName = `AsyncScriptLoader(${getDisplayName(WrappedComponent)})`;
+
+            static propTypes = {
+                asyncScriptOnLoad: PropTypes.func,
+            }
+
+            constructor(props) {
+                super(props);
+                this.asyncScriptLoaderHandleLoad = this.asyncScriptLoaderHandleLoad.bind(this);
+            }
+
+            componentDidMount() {
+                const key = this.asyncScriptLoaderGetScriptLoaderID();
+
+                if (globalName && typeof window[globalName] !== "undefined") {
+                    SCRIPT_MAP.set(scriptURL, { loaded: true, observers: new Map() });
+                }
+
+                if (SCRIPT_MAP.has(scriptURL)) {
+                    const entry = SCRIPT_MAP.get(scriptURL);
+                    if (entry.loaded || entry.errored) {
+                        this.asyncScriptLoaderHandleLoad(entry);
+                        return;
+                    }
+                    entry.observers.set(key, this.asyncScriptLoaderHandleLoad);
+                    return;
+                }
+
+                const observers = new Map();
+                observers.set(key, this.asyncScriptLoaderHandleLoad);
+                SCRIPT_MAP.set(scriptURL, {
+                    loaded: false,
+                    observers: observers,
+                });
+
+                const script = document.createElement("script");
+
+                script.src = scriptURL;
+                script.async = 1;
+
+                const callObserverFuncAndRemoveObserver = (func) => {
+                    if (SCRIPT_MAP.has(scriptURL)) {
+                        const mapEntry = SCRIPT_MAP.get(scriptURL);
+                        const observersMap = mapEntry.observers;
+
+                        for (const [obsKey, observer] of observersMap) {
+                            if (func(observer)) {
+                                observersMap.delete(obsKey);
+                            }
+                        }
+                    }
+
+                    // Remove the global callback if it exists.
+                    if (callbackName && typeof window !== "undefined") {
+                        delete window[callbackName];
+                    }
+                };
+
+                const scriptOnLoad = () => {
+                    const mapEntry = SCRIPT_MAP.get(scriptURL);
+                    mapEntry.loaded = true;
+                    callObserverFuncAndRemoveObserver( (observer) => {
+                        observer(mapEntry);
+                        return true;
+                    });
+                };
+
+                if (callbackName && typeof window !== "undefined") {
+                    // If user has provided a callbackName, allow the script being loaded
+                    // to trigger the callback.
+                    window[callbackName] = scriptOnLoad;
+                } else {
+                    // Otherwise listen fot the scripts onLoad event.
+                    script.onload = scriptOnLoad;
+                }
+
+                script.onerror = () => {
+                    const mapEntry = SCRIPT_MAP.get(scriptURL);
+                    mapEntry.errored = true;
+                    callObserverFuncAndRemoveObserver( (observer) => {
+                        observer(mapEntry);
+                        return true;
+                    });
+                };
+
+                // (old) MSIE browsers may call "onreadystatechange" instead of "onload"
+                script.onreadystatechange = () => {
+                    if (this.readyState === "loaded") {
+                        // wait for other events, then call onload if default onload hadn"t been called
+                        window.setTimeout(() => {
+                            if (SCRIPT_MAP.get(scriptURL).loaded !== true) {
+                                script.onload();
+                            }
+                        }, 0);
+                    }
+                };
+
+                document.body.appendChild(script);
+            }
+
+            componentWillUnmount() {
+                // Clean the observer entry
+                const mapEntry = SCRIPT_MAP.get(scriptURL);
+                if (mapEntry) {
+                    mapEntry.observers.delete(this.asyncScriptLoaderGetScriptLoaderID());
+                }
+            }
+
+            getComponent() {
+                return this.childComponent;
+            }
+
+            asyncScriptLoaderGetScriptLoaderID() {
+                if (!this.__scriptLoaderID) {
+                    this.__scriptLoaderID = "async-script-loader-" + idCount++;
+                }
+                return this.__scriptLoaderID;
+            }
+
+            asyncScriptLoaderHandleLoad(state) {
+                this.setState(state, this.props.asyncScriptOnLoad);
+            }
+
+            render() {
+                const { asyncScriptOnLoad, ...childProps } = this.props;
+                if (globalName && typeof window !== "undefined") {
+                    childProps[globalName] = typeof window[globalName] !== "undefined" ? window[globalName] : undefined;
+                }
+                return <WrappedComponent ref={(comp) => {this.childComponent = comp; }} {...childProps} />;
+            }
+        }
+
+        // Map functions to expose to the AsyncScriptLoader class
+        if (exposeFuncs) {
+            for (const funcToExpose of exposeFuncs) {
+                /* eslint-disable no-loop-func */
+                AsyncScriptLoader[funcToExpose] = function exposedFunc() {
+                    return this.childComponent[funcToExpose](...arguments);
+                };
+                /* eslint-enable no-loop-func */
+            }
+        }
+
+        return AsyncScriptLoader;
+    };
 }
